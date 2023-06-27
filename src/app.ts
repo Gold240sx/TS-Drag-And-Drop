1f79335a7204632bacd3a89946c13d0a1c9f3f12
Gold240sx <240designworks@gmail.com>
2023-06-27 15:13:05 -0500

    css loader added

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import { ProjectInput } from "./components(classes)/project-input"
 import { ProjectList } from "./components(classes)/project-list"
-import "app.css"
+import "./app.css"
 
 new ProjectInput()
 new ProjectList("active")
